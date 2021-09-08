37acd2669beb9e04ddf070491976a528218b6333
Ekene Ejeh <ekeneejeh@gmail.com>
2021-09-08 11:44:37 -0700

    finished the project and pushed to my Github

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // blackJackGame
 let player = {
-   name: "Ekene",
-   chips: 200,
+	name: "Ekene",
+	chips: 200,
 };
 let messageElement = document.getElementById("message-el");
 let cardsElement = document.getElementById("cards-el");
@@ -16,93 +16,50 @@ let playerElement = document.getElementById("player-el");
 playerElement.textContent = `${player.name}: $${player.chips}`;
 
 function getRandomCard() {
-   let getRandomCard = Math.floor(Math.random() * 13) + 1;
-   if (getRandomCard === 1) {
-      return 11;
-   } else if (getRandomCard > 10) {
-      return 10;
-   } else {
-      return getRandomCard;
-   }
+	let getRandomCard = Math.floor(Math.random() * 13) + 1;
+	if (getRandomCard === 1) {
+		return 11;
+	} else if (getRandomCard > 10) {
+		return 10;
+	} else {
+		return getRandomCard;
+	}
 }
 
 function startGame() {
-   isAlive = true;
-   firstCard = getRandomCard();
-   secondCard = getRandomCard();
-   cards = [firstCard, secondCard];
-   sum = firstCard + secondCard;
-   renderGame();
+	isAlive = true;
+	firstCard = getRandomCard();
+	secondCard = getRandomCard();
+	cards = [firstCard, secondCard];
+	sum = firstCard + secondCard;
+	renderGame();
 }
 
 function renderGame() {
-   cardsElement.textContent = "cards: ";
-   for (let index = 0; index < cards.length; index++) {
-      cardsElement.textContent += cards[index] + " ";
-   }
+	cardsElement.textContent = "cards: ";
+	for (let index = 0; index < cards.length; index++) {
+		cardsElement.textContent += cards[index] + " ";
+	}
 
-   if (sum <= 20) {
-      message = "Do You want to draw a new Card?";
-      isAlive = true;
-   } else if (sum === 21) {
-      message = "You have WON, you got BlackJack!!!";
-      hasBlackJack = true;
-   } else {
-      message = "Sorry You are out of the Game!!!";
-      isAlive = false;
-   }
-   sumElement.textContent = `Sum: ${sum}`;
-   messageElement.textContent = message;
+	if (sum <= 20) {
+		message = "Do You want to draw a new Card?";
+		isAlive = true;
+	} else if (sum === 21) {
+		message = "You have WON, you got BlackJack!!!";
+		hasBlackJack = true;
+	} else {
+		message = "Sorry You are out of the Game!!!";
+		isAlive = false;
+	}
+	sumElement.textContent = `Sum: ${sum}`;
+	messageElement.textContent = message;
 }
 
 function newCard() {
-   if (isAlive === true && hasBlackJack === false) {
-      let card = getRandomCard();
-      sum += card;
-      cards.push(card);
-      renderGame();
-   }
+	if (isAlive === true && hasBlackJack === false) {
+		let card = getRandomCard();
+		sum += card;
+		cards.push(card);
+		renderGame();
+	}
 }
-
-//practice time
-
-//objects
-let person = {
-   name: "Ekene",
-   age: 32,
-   country: "Canada",
-};
-
-let logData = () => {
-   console.log(
-      `${person.name} is ${person.age} years old and lives in ${person.country}`
-   );
-};
-
-logData();
-
-//if else statement
-let age = 67;
-
-if (age < 6) {
-   console.log("free");
-} else if (age > 6 && age <= 17) {
-   console.log("child discount");
-} else if (age > 18 && age <= 26) {
-   console.log("student discount");
-} else if (age > 27 && age <= 66) {
-   console.log("full price");
-} else {
-   console.log("senior citizen discount");
-}
-
-//loops and arrays
-let largeCountries = ["China", "India", "USA", "Indonesia", "Pakistan"];
-
-console.log("The 5 Largest Countries ib the World:");
-for (let index = 0; index < largeCountries.length; index++) {
-   const element = largeCountries[index];
-   console.log("- " + element);
-}
-
-//push, pop, unshift, shift
